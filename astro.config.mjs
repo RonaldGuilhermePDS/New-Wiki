937449d274b545cf4e768bcabff11d6a83fa8353
Ronald Guilherme <ronald.guilherme@chat2desk.com.br>
2023-04-14 19:48:41 -0300

    feat: add tailwind css structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import { SITE } from './src/config.mjs';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE.origin,
+  base: SITE.basePathname,
+  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+
+  output: 'static',
+
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
+});
